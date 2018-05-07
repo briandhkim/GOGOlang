dd5588c7fda0d939ed5ae5a53c45c5799b782c5e
briandhkim <w1057216@gmail.com>
2018-05-07 11:52:52 -0700

    formatting with utf-8

--- 02_numeralSys/numberLoop.go
+++ 02_numeralSys/numberLoop.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 200; i++ {
-		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+		// fmt.Printf("%d \t %b \t %x \n", i, i, i)
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+		// %q - utf8
 	}
 }
