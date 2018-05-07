6a9143862db7398a001b46275fb917c748f59997
briandhkim <w1057216@gmail.com>
2018-05-07 11:47:22 -0700

    for loop

--- 02_numeralSys/numberFormat.go
+++ 02_numeralSys/numberFormat.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%d - %b - %x", 42, 42, 42)
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
 	// %d - decimal
 	// %b - binary
 	// %x - hexadecimal
+	fmt.Printf("%d \t %b \t %#X", 42, 42, 42)
 }
