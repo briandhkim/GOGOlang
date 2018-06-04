537ae358629687a52967958b348532cdfcc2fa15
briandhkim <w1057216@gmail.com>
2018-06-04 00:37:47 -0700

    function intro

---
+++ 05_Functions/01_main/funcIntro.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/*
+can only have one `func main` in the entire program
+it needs to be inside `package main`
+
+functions:
+start with `func`
+receiver if there is one
+then function name
+then parenthesis
+
+	func sampleFunction() int{}
+	`int` is the return type
+*/
+
+func main() {
+	fmt.Println("hello world")
+}
