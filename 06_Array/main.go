43d4e539d28fe756b16a1696b7d18e4c98b7e6f1
briandhkim <w1057216@gmail.com>
2018-06-13 01:27:50 -0700

    array example three

--- 06_Array/main.go
+++ 06_Array/main.go
@@ -14,7 +14,8 @@ var x []string
 
 func main() {
 	// arrSample_1()
-	arrSample_2()
+	// arrSample_2()
+	arrSample_3()
 }
 
 func arrSample_1() {
@@ -33,10 +34,25 @@ func arrSample_1() {
 func arrSample_2() {
 	var x [100]int
 	fmt.Println(len(x))
-	fmt.Println(len(x))
 	fmt.Println(x[42])
 	for i := 0; i < 100; i++ {
 		x[i] = i
+	}
+	for i, v := range x {
+		fmt.Printf("%v - %T - %b\n", v, v, v)
+		if i > 50 {
+			break
+		}
+	}
+}
+
+func arrSample_3() {
+	//using byte type
+	var x [100]byte
+	fmt.Println(len(x))
+	fmt.Println(x[0])
+	for i := 0; i < 100; i++ {
+		x[i] = byte(i)
 	}
 	for i, v := range x {
 		fmt.Printf("%v - %T - %b\n", v, v, v)
