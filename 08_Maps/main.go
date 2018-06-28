f8354b034a6baa78109bacf68a86b63711f75182
briandhkim <w1057216@gmail.com>
2018-06-27 20:57:09 -0700

    starting maps section

---
+++ 08_Maps/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+/**
+maps:
+- key/value storage
+- a 'dictionary'
+- an unordered group of elements of one type, called the element type
+	- indexed by a set of unique keys of another type, called the key type
+	- vlue of an uninitialized map is nil
+
+a map is built on top of a hash table
+map is reference type
+	stores a pointer to the address where the data is actually stored
+
+**/ 
+
+func main(){
+
+}
