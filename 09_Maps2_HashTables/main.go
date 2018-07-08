4fb2e574965119a10733d925e2c4e74aeb0a4058
briandhkim <w1057216@gmail.com>
2018-07-08 16:29:56 -0700

    starting notes on hash talbe

--- 09_Maps2_HashTables/main.go
+++ 09_Maps2_HashTables/main.go
@@ -1,1 +1,21 @@
-main.go
+package main 
+
+import "fmt"
+
+/**
+Hash table:
+	type - unordered associative array
+	Search, Insert, Delete, Space -> O(n)
+hash tables advantages:
+	fast search
+
+in order of slowest to fastest in searching:
+	unsorted
+	sorted
+	categorized (buckets/slots)
+		(e.g. - words in As, Bs, Cs... Zs)
+**/
+
+func main(){
+
+}
